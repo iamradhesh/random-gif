abffdda56cb95f5f3f35f0748959a45fcea6b4c6
iamradhesh <radhesh185@gmail.com>
2023-07-13 00:32:42 +0530

    custom hook created

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,5 +1,45 @@
-const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+import React from "react";
+API_KEY="XSuJJiwo8S1osKCM5AVXH9ze2tDIe7EC"
+const RandomURL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+const TagURL=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+const useGif = (tag) => {
 
-const useGif = (tag) => {};
+const [gif,setGif]=useState('');
+
+const [loading,setLoading]=useState(false);
+
+  
+  
+async function fetchData()
+  {
+
+    setLoading(true);
+    
+    
+
+    const {data}=await axios.get(tag?(TagURL):(RandomURL));
+
+    const imgSrc=data.data.images.downsized_large.url;
+
+    console.log(imgSrc);
+
+    setGif(imgSrc);
+    setLoading(false);
+
+  }
+
+useEffect(()=>{
+    fetchData();
+  },[])
+
+  return {gif,loading,fetchData};
+  
+  
+  
+  
+  
+  
+ 
+};
 
 export default useGif;
